71e679612a08aa3067557a271457daa26053e622
Shashank Shekhar <sshekhar336@gmail.com>
2020-09-27 09:35:58 +0530

    Updates

---
+++ precache-manifest.504567b15de2727c70b06dbd0d3c95c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c2fb602783acc60435119810028ffda",
+    "url": "/url-viewer/index.html"
+  },
+  {
+    "revision": "7a50881d1b5681180162",
+    "url": "/url-viewer/static/css/main.39d06ac8.chunk.css"
+  },
+  {
+    "revision": "812b799e897a2fe21304",
+    "url": "/url-viewer/static/js/2.fcffeb66.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/url-viewer/static/js/2.fcffeb66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a50881d1b5681180162",
+    "url": "/url-viewer/static/js/main.c166ff44.chunk.js"
+  },
+  {
+    "revision": "777878c471e8c3a17ec9",
+    "url": "/url-viewer/static/js/runtime-main.c61a105d.js"
+  }
+]);
