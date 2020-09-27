28773fdde074e408c5dce9724a0ab07b569cfe88
Shashank Shekhar <sshekhar336@gmail.com>
2020-09-27 09:44:51 +0530

    Routing solved

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Login from './pages/Login/Login';
 import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route path="/dashboard" component={Dashboard} />
-      </Switch>
-    </Router>
+    <HashRouter basename='/'>
+      <div>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </div>
+    </HashRouter>
   );
 }
 
